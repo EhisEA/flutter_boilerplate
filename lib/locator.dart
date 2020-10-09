0d0bb06a772aa71a81467377f8f8c94a7d7b4919
Emmanuel Ehis <emmanueleayemere@gmail.com>
2020-10-09 16:03:45 +0100

    first commit

---
+++ lib/locator.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  
+// locator.registerLazySingleton(() =>AuthenticationService());
+
+// getIt.registerLazySingleton<Authentication>(() =>Authentication());
+// getIt.registerLazySingleton<Authentication>(() =>Authentication());
+
+}
